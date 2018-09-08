716914ac2beafd38bf10e5e4bddfc728f46fe022
sandrosalvato94 <sandrosalvato94@live.it>
2018-09-08 10:25:33 +0200

    Jump, JumpAndLink tests passed

--- Binary/Passed/JumpAndLink.asm
+++ Binary/Passed/JumpAndLink.asm
@@ -1,6 +1,6 @@
 seq r25, r25, r25 ; r25 <= 1
-sne r20, r25, r20 ; just to try 
-myloop:
+sne r20, r25, r20 ; just to try
+myloop: 
 add r1, r1, r25   ; r1 <= 1, 17 , 145
 add r1, r25, r1   ; r1 <= 2, 18 , 146
 add r1, r1, r1    ; r1 <= 4, 36 , 292
